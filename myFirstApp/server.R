680173eae72e44aa890fa49179a5f2e9262fbd72
Matiki <matiasbruzza@gmail.com>
2018-11-18 14:39:49 -0800

    changed app to create plot of uniform random points

--- myFirstApp/server.R
+++ myFirstApp/server.R
@@ -2,6 +2,24 @@ library(shiny)
 
 shinyServer(function(input, output) {
         
-        output$text1 = renderText(input$slider1)
-        
+        output$plot1 = renderPlot({
+                set.seed(20181118)
+                
+                npoints <- input$numeric
+                
+                minx <- input$sliderx[1]
+                maxx <- input$sliderx[2]
+                miny <- input$slidery[1]
+                maxy <- input$slidery[2]
+                
+                datax <- runif(npoints, minx, maxx)
+                datay <- runif(npoints, miny, maxy)
+                
+                xlab <- ifelse(input$show_xlab, "X axis", "")
+                ylab <- ifelse(input$show_ylab, "Y axis", "")
+                main <- ifelse(input$show_title, "Title", "")
+                
+                plot(datax, datay, xlab = xlab, ylab = ylab, main = main,
+                     xlim = c(-100, 100), ylim = c(-100, 100))
+        })
 })
--- myFirstApp/ui.R
+++ myFirstApp/ui.R
@@ -1,15 +1,26 @@
 library(shiny)
 
 shinyUI(fluidPage(
-        titlePanel("Slider App"),
+        titlePanel("Plot Rando Numbers"),
         sidebarLayout(
                 sidebarPanel(
-                        h3("Move the slider!"),
-                        sliderInput("slider1", "slide me!", 0, 100, 0)
+                        numericInput("numeric", 
+                                     "How many random numbers should be plotted?",
+                                     value = 1000, min = 1, max = 1000, step = 1),
+                        sliderInput("sliderx", "Pick min and max X values",
+                                    min = -100, max = 100, value = c(-50, 50)),
+                        sliderInput("slidery", "Pick min and max Y values",
+                                    min = -100, max = 100, value = c(-50, 50)),
+                        checkboxInput("show_xlab", "Show/hide X axis label", 
+                                      value = TRUE),
+                        checkboxInput("show_ylab", "Show/hide Y axis label", 
+                                      value = TRUE),
+                        checkboxInput("show_title", "Show/hide title")
                 ),
+                
                 mainPanel(
-                        h3("Slider value:"),
-                        textOutput("text1")
+                        h3("Graph of random points"),
+                        plotOutput("plot1")
                 )
         )
 ))
