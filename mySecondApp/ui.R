abb1072b57ba1b284726a2a3ce9f9ee9a29cd49b
Matiki <matiasbruzza@gmail.com>
2018-11-18 15:20:35 -0800

    added new plot with reactive() calculation

---
+++ mySecondApp/ui.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+shinyUI(fluidPage(
+        titlePanel("Predict HP from MPG"),
+        sidebarLayout(
+                sidebarPanel(
+                        sliderInput("sliderMPG", "What is MPG of car?", 
+                                    10, 35, value = 20),
+                        checkboxInput("showmodel1", "Show/hide Model 1", 
+                                      value = TRUE),
+                        checkboxInput("showmodel2", "Show/hide Model 2",
+                                      value = TRUE)
+                ),
+                mainPanel(
+                        plotOutput("plot1"),
+                        h3("Predicted HP from Model 1:"),
+                        textOutput("pred1"),
+                        h3("Predicted HP from Model 2:"),
+                        textOutput('pred2')
+                )
+        )
+))
